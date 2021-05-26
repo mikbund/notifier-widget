fc6be306c1a49fa66b1f97c60a568e7c8974aca4
Mikkel Bundgaard <mikkel@notfound.dk>
2021-05-26 14:58:37 +0200

    .

--- src/main/java/dk/notfound/notifier/widget/EventViewerWidget.java
+++ src/main/java/dk/notfound/notifier/widget/EventViewerWidget.java
@@ -43,7 +43,6 @@ public class EventViewerWidget {
             }
         });
 
-
         jButtonMuteEvents.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
